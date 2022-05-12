5d36f1d805dfb47328cf253072319416d06ce139
Vladislav Panov <vp@Vladislavs-MacBook-Air.local>
2022-05-12 02:51:27 +0200

    chore: changed asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: './',
+  assetPrefix: isProd ? '/nabrosok-2022/' : './',
   compiler: {
     styledComponents: true,
   },
