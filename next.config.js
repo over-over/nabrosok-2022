d89f79625ee4b5b5d7d92c1e0287b2db77a47c51
Vladislav Panov <vp@Vladislavs-MacBook-Air.local>
2022-05-12 03:14:03 +0200

    build: pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
-
 const isProd = process.env.NODE_ENV === 'production';
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isProd ? '/nabrosok-2022' : '/',
   assetPrefix: isProd ? '/nabrosok-2022/' : './',
   compiler: {
     styledComponents: true,
