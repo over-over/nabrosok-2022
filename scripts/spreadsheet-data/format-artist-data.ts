159a2e4aa330fca036fc47dbdaaf9312da52d32b
Vladislav Panov <vp@Vladislavs-MacBook-Air.local>
2022-05-13 04:11:11 +0200

    chore: new images

--- scripts/spreadsheet-data/format-artist-data.ts
+++ scripts/spreadsheet-data/format-artist-data.ts
@@ -2,6 +2,7 @@ import { writeFileSync, existsSync } from 'fs';
 import { join } from 'path';
 
 import data from '../../data/spreadsheet.json';
+import worksJSON from '../../data/works.json';
 import { TArtistDetails, TWorkDetails } from '../../src/shared/lib';
 
 const ARTISTS_OUTPUT_PATH = join(__dirname, '../../data/artists.json');
@@ -12,6 +13,10 @@ type TArtistData = Record<number, TArtistDetails>;
 
 let globalWorkId = 0;
 
+const newGlobalWorkId = Object.keys(worksJSON).length;
+let brokenWorkAmount = 0;
+let normalWorkId: number;
+
 const getArtistData = () => {
   let allWorks: Record<number, TWorkDetails> = {};
   const result: TArtistData = {};
@@ -19,13 +24,65 @@ const getArtistData = () => {
   data.table.rows.forEach((values, index) => {
     const workDetails: Record<number, TWorkDetails> = {};
     const works = values.c.slice(9);
-    let workId = 0;
     for (let workIndex = 0; workIndex < works.length; workIndex++) {
       if (workIndex % 8 === 0) {
         if (workIndex > 0) {
-          workId++;
+          // if (normalWorkId !== undefined) {
+          //   globalWorkId = normalWorkId;
+          //   normalWorkId = undefined;
+          // } else {
           globalWorkId++;
+          // }
         }
+        const brokenWork =
+          worksJSON[globalWorkId as unknown as keyof typeof worksJSON];
+        if (false && brokenWork && brokenWork.name !== String(works[workIndex].v)) {
+          // console.log(
+          //   String(works[workIndex].v),
+          //   String(works[workIndex + 7].v),
+          // );
+          // normalWorkId = globalWorkId + 1;
+          const brokenGlobalWorkId = newGlobalWorkId + brokenWorkAmount;
+          brokenWorkAmount++;
+
+          workDetails[String(brokenGlobalWorkId)] = {
+            id: String(brokenGlobalWorkId),
+            artistId: String(index),
+            name: String(works[workIndex].v),
+          };
+          const localPhoto = `/images/works/work-${brokenGlobalWorkId}.jpg`;
+          const localURI = existsSync(PUBLIC_FOLDER + localPhoto)
+            ? localPhoto
+            : '';
+          workDetails[brokenGlobalWorkId].photo = works[workIndex+1]?.v
+            ? {
+                externalURI: String(works[workIndex+1].v),
+                localURI,
+              }
+            : undefined;
+          workDetails[brokenGlobalWorkId].technique = works[workIndex+2]?.v
+            ? String(works[workIndex+2].v)
+            : undefined;
+
+          workDetails[brokenGlobalWorkId].year = works[workIndex+3]?.f
+            ? String(works[workIndex+3].f)
+            : undefined;
+
+          workDetails[brokenGlobalWorkId].description = works[workIndex+4]?.v
+            ? String(works[workIndex+4].v)
+            : undefined;
+
+          workDetails[brokenGlobalWorkId].size = works[workIndex+4]?.v
+            ? String(works[workIndex+4].v)
+            : undefined;
+
+          workDetails[brokenGlobalWorkId].auction = works[workIndex+4]?.f
+            ? { price: works[workIndex+4].f, link: '' }
+            : undefined;
+
+          break;
+        }
+
         workDetails[String(globalWorkId)] = {
           id: String(globalWorkId),
           artistId: String(index),
