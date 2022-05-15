b7642e8753d2aa50e1417bb0d4e9cd9ee6aea952
Vladislav Panov <vp@Vladislavs-MacBook-Air.local>
2022-05-15 12:44:07 +0200

    chore: images

--- scripts/spreadsheet-data/resize-images.ts
+++ scripts/spreadsheet-data/resize-images.ts
@@ -1,24 +1,35 @@
-import { fstat, readdirSync, writeFileSync } from 'fs';
+import { readdirSync, writeFileSync, existsSync } from 'fs';
 import { join } from 'path';
 import sharp from 'sharp';
 
-const IMAGES_FOLDER = join(__dirname, '../../public/images');
+const IMAGES_FOLDER = join(__dirname, '../../public/images-max');
 
 const resizeImages = async () => {
-  const images = [];
-  readdirSync(IMAGES_FOLDER + '/artists').forEach(item => {
-    images.push(join(IMAGES_FOLDER, '/artists', item));
-  });
-  readdirSync(IMAGES_FOLDER + '/works').forEach(item => {
-    images.push(join(IMAGES_FOLDER, '/works', item));
-  });
-
-  for (const path of images) {
-    const sharpResult = await sharp(path)
-      .resize(512, 512, { fit: 'cover' })
-      .jpeg({ quality: 85 })
-      .toBuffer();
-    writeFileSync(path, sharpResult);
+  try {
+    const images: string[] = [];
+    readdirSync(IMAGES_FOLDER + '/artists').forEach(item => {
+      if (item.includes('.jpg') || item.includes('.peg')) {
+        images.push(join(IMAGES_FOLDER, '/artists', item));
+      }
+    });
+    readdirSync(IMAGES_FOLDER + '/works').forEach(item => {
+      if (item.includes('.jpg') || item.includes('.peg')) {
+        images.push(join(IMAGES_FOLDER, '/works', item));
+      }
+    });
+    for (const path of images) {
+      const newPath = path.replace('images-max', 'images');
+      // if (!existsSync(newPath)) {
+      const sharpResult = await sharp(path)
+        .resize(512, 512, { fit: 'cover' })
+        .withMetadata()
+        .jpeg({ quality: 85 })
+        .toBuffer();
+      writeFileSync(newPath, sharpResult);
+      // }
+    }
+  } catch (e) {
+    console.log(e);
   }
 };
 
